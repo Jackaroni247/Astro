63c0f1d98ec8f640bf6df9e18aa0517a2911c21b
Jackson Harris <jkharris2023@gmail.com>
2026-04-26 20:27:11 -0500

    Update Transformations.cpp

--- Transformations.cpp
+++ Transformations.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){
-    CelestialObject test1("Bob", 1.0, 2.0, 3.5, 2.9, 17.1);
-    CelestialObject test2(2.3, 1.79, 17467178.18418, 16.1, 23.0);
-    test1.print();
-    test2.print();
+    CelestialObject bound1("Bound1", 1.0, 1.0, 1.0, -45, 16*15);
+    CelestialObject bound2("Bound1", 1.0, 1.0, 1.0, -10, 19*15);
+    bound1.eqTOgc();
+    bound1.printEQ();
+    bound1.printGC();
+    bound2.eqTOgc();
+    bound2.printEQ();
+    bound2.printGC();
     return 1;
 }
